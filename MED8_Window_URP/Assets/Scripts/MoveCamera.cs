ea537a725c47a01e9b361b8dde342a741fb512ff
Katrine <katrine.moerchsabra@gmail.com>
2026-02-12 15:26:09 +0100

    Changed math formulas and variables in c#

--- MED8_Window_URP/Assets/Scripts/MoveCamera.cs
+++ MED8_Window_URP/Assets/Scripts/MoveCamera.cs
@@ -5,34 +5,30 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
+    //----------------------------------------------------------------------------------------------------------//
+    // Variables
     [SerializeField] private UnityPythonConnector gameManager;
+    [SerializeField] private float verticalAngle;
+    [SerializeField] private float horiziontalAngle;
+
     //----------------------------------------------------------------------------------------------------------//
     // Functions
-
-    //Moving the camera
-    void MoveCam(float xCoordinate, float yCoordinate)
-    {
-        transform.position = new Vector3(NormalizeX(xCoordinate), NormalizeY(yCoordinate), transform.position.z);
-    }
 
     //Tilting the camera
     void TiltCam(float xCoordinate, float yCoordinate)
     {
-        float tiltNormalizedX = 30f + (yCoordinate - 0f) * ((-30f) - 30f) / (1f - 0f);
-        float tiltNormalizedY = -30f + (xCoordinate - 0f) * (30f - (-30f)) / (1f - 0f);
-        transform.rotation = Quaternion.Euler(tiltNormalizedX, tiltNormalizedY, 0f);
+        Vector2 normalizedValues = Normalize(xCoordinate, yCoordinate, verticalAngle, horiziontalAngle);
+        transform.rotation = Quaternion.Euler(normalizedValues.x, normalizedValues.y, 0f);
     }
 
     //Normalize x coordinates from python to unity
-    float NormalizeX(float xCoordinate)
+    Vector2 Normalize(float xCoordinate, float yCoordinate, float vertical, float horizontal)
     {
-        return -4f + (xCoordinate - 0f) * (4f - (-4f)) / (1f - 0f);
-    }
 
-    //Normalize y coordinates from python to unity
-    float NormalizeY(float yCoordinate)
-    {
-        return 0.5f + (yCoordinate - 0f) * (3.5f - 0.5f) / (1f - 0f);
+        return new Vector2(
+            -vertical + (yCoordinate - 0f) * (vertical - (-vertical)) / (1f - 0f),
+            -horizontal + (xCoordinate - 0f) * (horizontal - (-horizontal)) / (1f - 0f)
+            );
     }
 
     //----------------------------------------------------------------------------------------------------------//
@@ -45,13 +41,12 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            float randomFloat = UnityEngine.Random.Range(0f, 1f);
-            MoveCam(randomFloat, randomFloat);
-        }
-        //MoveCam(gameManager.receivedValue.x, gameManager.receivedValue.y);
         TiltCam(gameManager.receivedValue.x, gameManager.receivedValue.y);
+
+        // Rounding the values to 2 decimals
+        // float tempx = (float)Math.Round(gameManager.receivedValue.x, 2);
+        // float tempy = (float)Math.Round(gameManager.receivedValue.y, 2);
+        // TiltCam(tempx, tempy);
     }
     //----------------------------------------------------------------------------------------------------------//
 }
