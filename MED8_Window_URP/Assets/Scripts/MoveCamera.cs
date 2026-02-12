b99e3e7d3d2d79198acb870d54b40260ced34f53
Katrine <katrine.moerchsabra@gmail.com>
2026-02-12 12:26:22 +0100

    First test
    - Added formula to convert python output to untiy coordinates as input
    - Added TiltCam function for temporary testing

--- MED8_Window_URP/Assets/Scripts/MoveCamera.cs
+++ MED8_Window_URP/Assets/Scripts/MoveCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
+    [SerializeField] private UnityPythonConnector gameManager;
     //----------------------------------------------------------------------------------------------------------//
     // Functions
 
@@ -12,6 +13,14 @@ public class MoveCamera : MonoBehaviour
     void MoveCam(float xCoordinate, float yCoordinate)
     {
         transform.position = new Vector3(NormalizeX(xCoordinate), NormalizeY(yCoordinate), transform.position.z);
+    }
+
+    //Tilting the camera
+    void TiltCam(float xCoordinate, float yCoordinate)
+    {
+        float tiltNormalizedX = 30f + (yCoordinate - 0f) * ((-30f) - 30f) / (1f - 0f);
+        float tiltNormalizedY = -30f + (xCoordinate - 0f) * (30f - (-30f)) / (1f - 0f);
+        transform.rotation = Quaternion.Euler(tiltNormalizedX, tiltNormalizedY, 0f);
     }
 
     //Normalize x coordinates from python to unity
@@ -41,6 +50,8 @@ public class MoveCamera : MonoBehaviour
             float randomFloat = UnityEngine.Random.Range(0f, 1f);
             MoveCam(randomFloat, randomFloat);
         }
+        //MoveCam(gameManager.receivedValue.x, gameManager.receivedValue.y);
+        TiltCam(gameManager.receivedValue.x, gameManager.receivedValue.y);
     }
     //----------------------------------------------------------------------------------------------------------//
 }
