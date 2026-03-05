d2a2856ef565acf00f693c0530cfdbd9241cabdf
Acroh <fmalle20@student.aau.dk>
2026-03-05 10:29:17 +0100

    Made rotation into movement
    - simplyfied the code
    - Now moves the camera instead of rotates it

--- MED8_Window_URP/Assets/Scripts/MoveCamera.cs
+++ MED8_Window_URP/Assets/Scripts/MoveCamera.cs
@@ -8,51 +8,35 @@ public class MoveCamera : MonoBehaviour
     //----------------------------------------------------------------------------------------------------------//
     // Variables
     [SerializeField] private UnityPythonConnector gameManager;
-    [SerializeField] private float verticalAngle;
-    [SerializeField] private float horizontalAngle;
+    [SerializeField] private float horizontalRange = 2f; // movement left/right
+    [SerializeField] private float verticalRange = 2f;   // movement up/down
+    [SerializeField] private float depth = 0f;           // optional forward/back
+
+    private Vector3 startPosition = new Vector3(0, 0, -5);
 
     //----------------------------------------------------------------------------------------------------------//
-    // Functions
-
-    //Tilting the camera
-    void TiltCam(float xCoordinate, float yCoordinate)
-    {
-        Vector2 normalizedValues = Normalize(xCoordinate, yCoordinate, verticalAngle, horizontalAngle);
-        transform.rotation = Quaternion.Euler(normalizedValues.x, normalizedValues.y, 0f);
-    }
-
-    void MoveCam(float x, float y, float z)
-    {
-        
-    }
-
-    //Normalize x coordinates from python to unity
-    Vector2 Normalize(float xCoordinate, float yCoordinate, float vertical, float horizontal)
-    {
-
-        return new Vector2(
-            -vertical + (yCoordinate - 0f) * (vertical - (-vertical)) / (1f - 0f),
-            -horizontal + (xCoordinate - 0f) * (horizontal - (-horizontal)) / (1f - 0f)
-            );
-    }
-
-    void SetVerticalAngle()
-    {
-        verticalAngle = (horizontalAngle / gameManager._FOV) * -1;
-    }
-
-    //----------------------------------------------------------------------------------------------------------//
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
+    
+    // Functions
+
+    void MoveCam(float x, float y)
+    {
+        float posX = Mathf.Lerp(horizontalRange, -horizontalRange, x);
+        float posY = Mathf.Lerp(verticalRange, -verticalRange, y);
+
+        transform.localPosition = startPosition + new Vector3(posX, posY, 0f);
+    }
+
+    
     //----------------------------------------------------------------------------------------------------------//
     // Update is called once per frame
     void Update()
     {
-        SetVerticalAngle();
-        TiltCam(gameManager.receivedValue.x, gameManager.receivedValue.y);
+        MoveCam(gameManager.receivedValue.x, gameManager.receivedValue.y);
 
         // Rounding the values to 2 decimals
         // float tempx = (float)Math.Round(gameManager.receivedValue.x, 2);
